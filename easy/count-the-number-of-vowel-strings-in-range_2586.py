e08524414178e2fa05fddf5b4f7e41b5c73f9cc4
Timofey <tima_yakovishin@mail.ru>
2023-04-17 09:14:28 +0300

    [add] solve 1 problem

--- easy/count-the-number-of-vowel-strings-in-range_2586.py
+++ easy/count-the-number-of-vowel-strings-in-range_2586.py
@@ -1,27 +1,22 @@
+from functools import reduce
+
+
+
 class List(list):
     ...
 
 
-# My solution
+
 
 class Solution:
     def vowelStrings(self, words: List[str], left: int, right: int) -> int:
-        count = 0
-        result = 0
-        arr = words[left:right + 1]
-        while count <= len(arr):
+        def count_vowel(string):
             vowel = dict(zip(('a', 'e', 'i', 'o', 'u'), ('a', 'e', 'i', 'o', 'u')))
-            if vowel.get(arr[count][0]) and vowel.get(arr[count][-1]):
-                result += 1
-            count += 1
-        return result
-
+            if vowel.get(string[0]) and vowel.get(string[-1]):
+                return 1
+            return 0
+        return reduce(lambda acc, x: acc + count_vowel(x), words[left:right + 1], 0)
     
 
-# Solution Spaulding_
-class Solution:
-    def vowelStrings(self, words: List[str], left: int, right: int) -> int:
-
-        vowels = set('aeiou')
-        
-        return sum({w[0],w[-1]}.issubset(vowels) for w in words[left:right+1])
+sol = Solution()
+print(sol.vowelStrings(words = ["are","amy","u"], left = 0, right = 2))
