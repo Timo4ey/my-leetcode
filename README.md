733b0126b138a97de59e8f41d77c484529466b4c
Timofey <tima_yakovishin@mail.ru>
2023-05-19 09:19:13 +0300

    [add] easy: '1047. Remove All Adjacent Duplicates In String' <type:stack>

--- README.md
+++ README.md
@@ -48,6 +48,7 @@ Try to solve every day!!!
 4. [283. Move Zeroes](easy/move-zeroes_283.py)
 4. [1700. Number of Students Unable to Eat Lunch](easy/number-of-students-unable-to-eat-lunch_1700.py)
 4. [20. Valid Parentheses](easy/valid-parentheses_20.py)
+4. [1047. Remove All Adjacent Duplicates In String](easy/remove-all-adjacent-duplicates-in-string_1047.py)
 
 ### Medium Tasks
 1. [142. Linked List Cycle II](medium/linked-list-cycle-ii_142.py)
---
+++ easy/remove-all-adjacent-duplicates-in-string_1047.py
@@ -0,0 +1,52 @@
+# You are given a string s consisting of lowercase English letters.
+# A duplicate removal consists of choosing two adjacent and equal letters and removing them.
+
+# We repeatedly make duplicate removals on s until we no longer can.
+
+# Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+
+# Example 1:
+
+# Input: s = "abbaca"
+# Output: "ca"
+# Explanation: 
+# For example, in "abbaca" we could remove "bb" since the letters are adjacent and equal, and this is the only possible move.  The result of this move is that the string is "aaca", of which only "aa" is possible, so the final string is "ca".
+# Example 2:
+
+# Input: s = "azxxzy"
+# Output: "ay"
+
+# ---------------------------------------Runtime 515 ms Beats 5.3% Memory 17.1 MB Beats 24.43%---------------------------------------
+
+# Type: Stack
+
+
+from functools import reduce
+
+# My solution 
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = list(s)
+        i = 0
+        r = 1
+        while r <= len(stack) - 1:
+            num1 = stack[i]
+            num2 = stack[r]
+            if num1 == num2:
+                stack.pop(i)
+                stack.pop(i)
+                if i:
+                    i -= 1
+                    r -= 1
+            else:
+                i += 1
+                r += 1
+
+        return ''.join(stack)
+
+
+
+# Solution @lee215
+class Solution:
+    def removeDuplicates(self, S):
+        return reduce(lambda s, c: s[:-1] if s[-1:] == c else s + c, S)
