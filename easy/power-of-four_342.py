b374f81f7dd43a7d2608a956109bcca0f897762b
Timofey <tima_yakovishin@mail.ru>
2023-05-29 08:38:49 +0300

    [add] easy: '326. Power of Three' <type:Recursion>, '231. Power of Two' <type:Recursion>, '342. Power of Four' <type:Recursion>

---
+++ easy/power-of-four_342.py
@@ -0,0 +1,29 @@
+
+# Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+# An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+
+# Example 1:
+
+# Input: n = 16
+# Output: true
+# Example 2:
+
+# Input: n = 5
+# Output: false
+# Example 3:
+
+# Input: n = 1
+# Output: true
+
+# ---------------------------------------Runtime 48 ms Beats 33.13% Memory 16.4 MB Beats 9.18%---------------------------------------
+
+# My solution
+# Complexity O(n)
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 1:
+            return n == 1
+        return self.isPowerOfFour(n/4)
---
+++ easy/power-of-three_326.py
@@ -0,0 +1,33 @@
+# Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+# An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+ 
+
+# Example 1:
+
+# Input: n = 27
+# Output: true
+# Explanation: 27 = 33
+# Example 2:
+
+# Input: n = 0
+# Output: false
+# Explanation: There is no x where 3x = 0.
+# Example 3:
+
+# Input: n = -1
+# Output: false
+# Explanation: There is no x where 3x = (-1).
+
+
+# ---------------------------------------Runtime 118 ms Beats 17.5% Memory 16.3 MB Beats 40.15%---------------------------------------
+
+# My solution
+# Complexity O(n)
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 1:
+            return n == 1
+        return self.isPowerOfThree(n/3)
