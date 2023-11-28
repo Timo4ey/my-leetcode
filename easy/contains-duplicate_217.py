107b69a36e4b5708d4df5e353f57cac1b5e4deff
Timofey <tima_yakovishin@mail.ru>
2023-11-28 08:57:13 +0300

    [fix] update solution and fix description

--- easy/contains-duplicate_217.py
+++ easy/contains-duplicate_217.py
@@ -1,20 +1,29 @@
-class List(list):
-    ...
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 
-# My solution
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+
+# ---------------------------------------Runtime 459 ms Beats 95.54% Memory 31.8 MB Beats 32.63%---------------------------------------
+
+from typing import List
 
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        double = {}
+        double = set()
         for i in nums:
-            if double.get(i):
+            if i in double:
                 return True
-            double[i] = double.get(i, 0) + 1
+            double.add(i)
         return False
-
-
-s = Solution()
-print(s.containsDuplicate([0, 4, 5, 0, 3, 6]))
-print(s.containsDuplicate([1, 2, 3, 1]))
